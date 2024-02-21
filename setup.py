dc2000f08b14675f6331ca8b6ae671415e75a519
sank.eth <sankethchebbi@gmail.com>
2024-02-21 12:06:53 +0530

    asas

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name="trussme",
+    name="justruss",
     version="0.0.1",
     description="Truss construction and analysis",
     long_description=readme,
     license=license,
     author="Christopher McComb",
     author_email="ccmcc2012@gmail.com",
-    url="https://github.com/cmccomb/TrussMe",
+    url="https://github.com/sankethchebbi/istrac",
     install_requires=["numpy", "pandas", "tabulate", "matplotlib", "scipy"],
     packages=find_packages(exclude="tests"),
 )
